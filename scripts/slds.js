74fd4317c58cff98cbc2fadf808c4fe3c4b82b5a
Robin Mead <robin.mead@secretescapes.com>
2019-04-28 11:15:17 +0100

    Intial Load
    Intital test load of files

---
+++ scripts/slds.js
@@ -0,0 +1,195 @@
+// Accordion
+$(document).on('click', '.slds-accordion__summary-action', function(){
+  $(this).closest(".slds-accordion__section").toggleClass("slds-is-open");
+});
+
+// Activity Timeline
+$(document).on('click', '.slds-timeline__item_task .slds-button', function(){
+  $(this).closest(".slds-timeline__item_expandable").toggleClass('slds-is-open');
+});
+
+// Alert
+$(document).on('click', '.slds-notify__close', function(){
+  $(this).closest(".slds-notify").remove();
+});
+
+// App Launcher 
+$(document).on('click', '.slds-global-header_container .slds-icon-waffle_container', function(){
+  $(".slds-app-launcher").toggleClass('slds-hide');
+  $(".slds-backdrop").addClass('slds-backdrop_open');
+});
+
+// Carousel
+$(document).on('click', '.slds-carousel__indicator-action', function(){
+  var n = $(".slds-carousel__indicator-action").index(this);
+  $(".slds-carousel__indicator-action").removeClass('slds-is-active');
+  $(this).addClass('slds-is-active');
+  $(this).closest(".slds-carousel").find(".slds-carousel__panels").css('transform', 'translateX(-' + n*100 + '%)');
+});
+
+// Chat
+$(".slds-chat-list").find(".slds-chat-listitem").each(function() {
+  var n = $(".slds-chat-listitem").index(this) + 2;
+  var sel = '.slds-chat-listitem:nth-child(' + n + ')';
+  if($(this).find(".slds-chat-message__meta").html() == $(sel).find(".slds-chat-message__meta").html()){
+    $(this).find(".slds-chat-message__meta").toggleClass('slds-hide');
+  }
+});
+
+// Color Picker
+$(document).on('click', '.slds-color-picker .slds-swatch', function(){
+  $(this).closest(".slds-color-picker").find(".slds-color-picker__selector").toggleClass('slds-hide');
+});
+
+$(document).on('click', '.slds-color-picker__swatch-trigger', function(){
+  var color = $(this).find(".slds-swatch").css("background-color");
+  $(this).closest(".slds-color-picker").find(".slds-color-picker__summary").find(".slds-swatch").css("background-color", color);
+  $(this).closest(".slds-color-picker").find(".slds-color-picker__summary").find(".slds-input").val(rgb2hex(color));
+  $(this).closest(".slds-color-picker__selector").addClass('slds-hide');
+});
+
+$(document).on('keyup', '.slds-color-picker__summary-input input', function(){
+  var color = $(this).val();
+  $(this).closest(".slds-color-picker").find(".slds-color-picker__summary").find(".slds-swatch").css("background-color", color);
+  $(this).closest(".slds-color-picker").find(".slds-color-picker__selector").addClass('slds-hide');
+});
+
+
+// Combo Box
+// Combo Box
+// Combo Box
+
+// Date Picker
+$(document).on('click', '.hasDatepicker', function(){
+  $(".ui-datepicker").addClass('slds-datepicker slds-dropdown slds-dropdown_medium slds-dropdown_left');
+  // Header Section
+  $(".ui-datepicker-header").addClass('slds-datepicker__filter slds-grid slds-p-left_xxx-small');
+  $(".ui-datepicker-prev").addClass('slds-button slds-button_neutral slds-col slds-size_1-of-4');
+  $(".ui-datepicker-next").addClass('slds-button slds-button_neutral  slds-col slds-size_1-of-4');
+  $(".ui-datepicker-title").addClass('slds-text-heading_small slds-col slds-size_1-of-2 slds-align_absolute-center');
+  // Calendar Section
+  $(".ui-datepicker-calendar").addClass('slds-datepicker__month');
+  $(".ui-state-default").addClass('slds-day');
+  $(".ui-datepicker-current-day").addClass('slds-is-selected'); 
+});
+$(document).on('click', '.ui-datepicker-prev', function(){
+  $(".ui-datepicker").addClass('slds-datepicker slds-dropdown slds-dropdown_medium slds-dropdown_left');
+  // Header Section
+  $(".ui-datepicker-header").addClass('slds-datepicker__filter slds-grid slds-p-left_xxx-small');
+  $(".ui-datepicker-prev").addClass('slds-button slds-button_neutral slds-col slds-size_1-of-4');
+  $(".ui-datepicker-next").addClass('slds-button slds-button_neutral  slds-col slds-size_1-of-4');
+  $(".ui-datepicker-title").addClass('slds-text-heading_small slds-col slds-size_1-of-2 slds-align_absolute-center');
+  // Calendar Section
+  $(".ui-datepicker-calendar").addClass('slds-datepicker__month');
+  $(".ui-state-default").addClass('slds-day');
+  $(".ui-datepicker-current-day").addClass('slds-is-selected'); 
+});
+$(document).on('click', '.ui-datepicker-next', function(){
+  $(".ui-datepicker").addClass('slds-datepicker slds-dropdown slds-dropdown_medium slds-dropdown_left');
+  // Header Section
+  $(".ui-datepicker-header").addClass('slds-datepicker__filter slds-grid slds-p-left_xxx-small');
+  $(".ui-datepicker-prev").addClass('slds-button slds-button_neutral slds-col slds-size_1-of-4');
+  $(".ui-datepicker-next").addClass('slds-button slds-button_neutral  slds-col slds-size_1-of-4');
+  $(".ui-datepicker-title").addClass('slds-text-heading_small slds-col slds-size_1-of-2 slds-align_absolute-center');
+  // Calendar Section
+  $(".ui-datepicker-calendar").addClass('slds-datepicker__month');
+  $(".ui-state-default").addClass('slds-day');
+  $(".ui-datepicker-current-day").addClass('slds-is-selected'); 
+});
+
+// Docked Composer
+$(document).on('click', '.slds-docked-composer .minimize', function(){
+  $(this).closest("section").toggleClass('slds-is-open slds-is-closed');
+});
+$(document).on('click', '.slds-docked-composer .close', function(){
+  $(this).closest(".slds-docked_container").toggleClass('slds-hide');
+});
+
+// Dueling Picklist
+$( function() {
+  $( ".slds-dueling-list__options ul" ).css('min-height','40px')
+  $( ".slds-dueling-list__options ul" ).sortable({
+    connectWith: ".slds-listbox",
+    placeholder: "slds-drop-zone_drag__slot"
+  });
+  $(".slds-dueling-list__options ul").disableSelection();
+});
+
+// Expandable Section
+$(document).on('click', '.slds-section .slds-section__title-action', function(){
+  $(this).closest(".slds-section").toggleClass('slds-is-open');
+});
+
+// Global Header
+$(document).on('click', '.slds-global-header__item_search button', function(){
+  $(this).closest(".slds-global-header__item_search").find("input").val('');
+});
+$(document).on('keydown', '.slds-global-header__item_search input', function(){
+  if (event.keyCode == 13) {
+    this.form.submit();
+    return false;
+  }
+});
+$(document).on('click', '.slds-global-actions__notifications', function(){
+  $(this).closest(".slds-global-actions__item").find(".slds-popover").toggleClass('slds-hide');
+});
+
+// Menus
+$(document).on('mouseenter','.slds-dropdown-trigger_hover', function(){
+  $(this).closest(".slds-dropdown-trigger").addClass('slds-is-open');
+}).on('mouseleave','.slds-dropdown-trigger_hover', function(){
+  $(this).closest(".slds-dropdown-trigger").removeClass('slds-is-open');
+});
+$(document).on('click','.slds-dropdown-trigger_click', function(){
+  $(this).closest(".slds-dropdown-trigger").toggleClass('slds-is-open');
+});
+
+// Modal
+$(document).on('click', '.slds-modal__close', function(){
+  $(".slds-modal").toggleClass('slds-hide');
+  $(".slds-backdrop").removeClass('slds-backdrop_open');
+});
+
+// Notifications
+$(document).on('click', '.slds-notification__close', function(){
+  $(this).closest(".slds-notification").remove();
+});
+
+// Panel
+$(document).on('click', '.slds-panel__close', function(){
+  $(this).closest(".slds-panel").toggleClass('slds-is-open');
+});
+
+// Pills
+$(document).on('click', '.slds-pill__remove', function(){
+  $(this).closest(".slds-pill").remove();
+});
+
+// Popover
+$(document).on('click', '.slds-popover__close', function(){
+  $(this).closest(".slds-popover").toggleClass('slds-hide');
+});
+
+// Scoped Tabs
+$(document).on('click', '.slds-tabs_scoped__item', function(){
+  var n = $(".slds-tabs_scoped__item").index(this)
+  $(".slds-tabs_scoped__item").removeClass('slds-is-active');
+  $(this).addClass('slds-is-active');
+  $(this).closest(".slds-tabs_scoped").find(".slds-tabs_scoped__content").each(function( index ) {
+    $(this).removeClass('slds-show');
+    $(this).removeClass('slds-hide');
+    if(index==n){$(this).addClass('slds-show')} else {$(this).addClass('slds-hide')};
+  });
+});
+
+// Vertical Tabs
+$(document).on('click', '.slds-vertical-tabs__nav-item', function(){
+  var n = $(".slds-vertical-tabs__nav-item").index(this)
+  $(".slds-vertical-tabs__nav-item").removeClass('slds-is-active');
+  $(this).addClass('slds-is-active');
+  $(this).closest(".slds-vertical-tabs").find(".slds-vertical-tabs__content").each(function( index ) {
+    $(this).removeClass('slds-show');
+    $(this).removeClass('slds-hide');
+    if(index==n){$(this).addClass('slds-show')} else {$(this).addClass('slds-hide')};
+  });
+});
---
+++ scripts/utilities.js
@@ -0,0 +1,121 @@
+// Cookie Functions
+function createCookie(cname,value,days) {
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 *1000));
+      var expires = "; expires=" + date.toGMTString();
+  } else {
+      var expires = "";
+  }
+  document.cookie = cname + "=" + value + expires + "; path=/";
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function eraseCookie(cname) {
+  createCookie(cname,"",-1);
+}
+
+// Global Autocomplete function 
+function globalSearch(d,c){
+  $(c).autocomplete({
+    source: d,
+    create: function( event, ui ) {
+      $(".ui-menu").addClass('slds-listbox slds-listbox_vertical slds-dropdown slds-dropdown_large');
+      $(".ui-menu-item").addClass('slds-listbox__item');
+      $(".ui-menu-item-wrapper").addClass('slds-media slds-listbox__option slds-listbox__option_plain slds-media_small');
+      $(".ui-helper-hidden-accessible").hide();
+    },
+    open: function( event, ui ) {
+      $(".ui-menu").addClass('slds-listbox slds-listbox_vertical slds-dropdown slds-dropdown_large');
+      $(".ui-menu-item").addClass('slds-listbox__item');
+      $(".ui-menu-item-wrapper").addClass('slds-media slds-listbox__option slds-listbox__option_plain slds-media_small');
+      $(".ui-helper-hidden-accessible").hide();
+    }
+  });
+}
+
+// Load Global Header - Email Builder
+$.get( "../components/global-header-email.htm", function(data) {
+  $("#global-header__email").html(data);
+  $(".slds-global-header_container nav li").removeClass('slds-is-active');
+  $('.slds-global-header_container nav li:nth-child(' + $("#global-header__email").attr('aria-label') + ')').addClass('slds-is-active');
+  var obj = JSON.parse(getCookie('sessionData'));
+  $(".slds-avatar img").attr('title', obj.Username);
+  $(".slds-avatar img").attr('alt', obj.Username);
+  $(".slds-avatar img").attr('src', obj.Avatar);
+  if(obj["AccountStatus"]=='Admin'){$(".slds-global-actions__setup").closest(".slds-global-actions__item").removeClass('slds-hide')}
+});
+$(document).on('keypress', '#global-header__email .slds-global-header__item_search input', function(){
+  var emailTags = [ "c++", "java", "Robin", "Rose"];
+  globalSearch(emailTags,'.slds-global-header__item_search input');
+});
+
+// Load App Launcher 
+$.get( "../components/app-launcher.htm", function(data) {
+  $("#app-launcher").html(data);
+});
+
+// Load Color Picker
+$(document).ready(function() {
+  // 7 per row
+  $.each( [ "#ff0000", "#00ff00", "#0000ff", "#00ff00", "#0000ff", "#0000ff" ], function( i, value ){
+    var html = '<li class="slds-color-picker__swatch" role="presentation"><a class="slds-color-picker__swatch-trigger" href="#" role="option" tabindex="0"><span class="slds-swatch" style="background: ' + value + ';"><span class="slds-assistive-text">' + value + '</span></span></a></li>'
+    $( ".slds-color-picker__swatches" ).append(html);
+  });
+});
+
+
+// Function to convert colors
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+  function hex(x) {
+    return ("0" + parseInt(x).toString(16)).slice(-2);
+  }
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+// Function to determine campaign name
+function nameCampaign(){
+  var a = $("#input_audience").val();
+  var d = $("#input_send-date").val();
+  var dt = d.replace(/-/g,'');
+  var day = '_' + $.datepicker.formatDate('D', new Date(d)) + '_';
+  var b = $("#input_brand").val();
+  var s = $("#input_source").val() + '_';
+  var t = $("#input_send-time").val();
+  var o = $("#input_offer").val();
+  var i = $("#input_id").val();
+  if(b.indexOf('_')>-1 && i!=''){
+    var name = 'PARTNERS_' + a + dt + s + b + day + o + i;
+  } else if (i!=''){
+    var name = a + dt + s + b + day + o + i;
+  } 
+  $("#input_campaign").val(name.toUpperCase());
+  $(".slds-box").html('<p>' + name.toUpperCase() + '</p>');
+  var url = "https://pages.email.secretescapes.com/emailbuilder/campaign-validation?id=" + name;
+  $.getJSON(url, function(data) {
+    if(data.length>0){
+      $(".slds-box").css('border-color','#ff0000');
+      $(".error__campaign").removeClass('slds-hide');
+      $("#wrapper button").prop( "disabled", true );
+    } else {
+      $(".slds-box").css('border-color','');
+      $(".error__campaign").addClass('slds-hide');
+      $("#wrapper button").prop( "disabled", false);
+    }
+  });
+}
