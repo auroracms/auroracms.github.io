945455fddd0ec7e4e3c2bdeef3c7eea8ba345ebd
Robin Mead <robin.mead@secretescapes.com>
2019-06-02 15:32:27 +0100

    Update slds.js

--- scripts/slds.js
+++ scripts/slds.js
@@ -182,6 +182,17 @@ $(document).on('click', '.slds-tabs_scoped__item', function(){
   });
 });
 
+// Tree
+$(document).on('click', '.slds-tree__item button', function(){
+  var status = $(this).closest("li").attr('aria-expanded');
+  if(status='true'){
+    $(this).closest("li").attr('aria-expanded','false');
+  } else {
+    $(this).closest("li").attr('aria-expanded','true');
+  }
+});
+
+
 // Vertical Tabs
 $(document).on('click', '.slds-vertical-tabs__nav-item', function(){
   var n = $(".slds-vertical-tabs__nav-item").index(this)
